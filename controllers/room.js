a10472c640f6c3782731d7a833849470eb68baee
dian prasetyo <dipra94@gmail.com>
2019-11-08 23:30:35 +0700

    Update room.js

--- controllers/room.js
+++ controllers/room.js
@@ -23,3 +23,15 @@ exports.getRoom = (req, res) => {
         where:{id: req.params.id},
     }).then(res.send(req.body))
 }
+ 
+ //DELETE ROOMS
+exports.deleteRoom = (req, res) => {
+    const {room_id} = req.params
+    room.destroy({
+        where : {id: room_id}
+    }).then(result => res.send({
+        id: room_id,
+        message : "room dihapus"
+    })
+    )
+}
